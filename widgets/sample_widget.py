cb6b7581445bcc7541ff990ea922404cebba0467
kevin6525 <kevin6523@gmail.com>
2024-01-20 21:36:58 -0800

    Add layouts to buttons on sample widget

--- widgets/sample_widget.py
+++ widgets/sample_widget.py
@@ -1,6 +1,15 @@
-from PySide6.QtWidgets import QWidget, QPushButton
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout
 
 class SampleWidget (QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Sample Widget")
+
+        button1 = QPushButton("Button1")
+        button2 = QPushButton("Button2")
+
+        button_layout = QVBoxLayout()
+        button_layout.addWidget(button1)
+        button_layout.addWidget(button2)
+        
+        self.setLayout(button_layout)
